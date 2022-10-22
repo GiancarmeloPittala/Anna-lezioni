00118e506f26165f29c4c4c071833193a460680d
unknown <giancarmelo.pittala@gmail.com>
2022-10-22 13:06:37 +0200

    lezione 1

---
+++ src/components/pokemon/ui/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>
+      {{ name }}
+    </h2>
+    <div>
+      {{details}}
+    </div>
+  </div>
+</template>
+
+<script setup >
+  import { ref, onMounted } from 'vue';
+
+  const props = defineProps(['pokemon']);
+  const {name, url } = props.pokemon
+  const details = ref({})
+
+  onMounted( async () => {
+    const response = await fetch(url);
+    details.value = await response.json();
+  })
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue';
+  import PokemonCard from "../components/pokemon/ui/Card.vue"
+  
+  let pokemons = ref("888")
+
+  async function func1(){
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon');
+
+    pokemons.value = await data.json();
+  }
+
+ func1()
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div v-for="pokemon of pokemons.results" 
+      :key="pokemon.name">
+
+      <PokemonCard :pokemon="pokemon" />
+      
+    </div>
+  </div>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
